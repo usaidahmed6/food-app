dfe0426a97fffd1312ed33c6b797661bb4d4ddde
usaid <usaidahmed61524@gmail.com>
2023-10-08 13:12:56 +0500

    first commit

---
+++ js/admin.js
@@ -0,0 +1,95 @@
+import { auth, collection, db, doc, getDoc, getDocs, onAuthStateChanged, signOut, updateDoc } from "../config/Firebase.js";
+
+
+window.addEventListener("load", (event) => {
+    getUsers()
+
+    onAuthStateChanged(auth, async (user) => {
+        if (user) {
+            const uid = user.uid;
+            // console.log('user is signed in ===========>', uid);
+            const docRef = doc(db, "users", uid);
+            const docSnap = await getDoc(docRef);
+            console.log("Document data:", docSnap.data());
+            const userDate = docSnap.data();
+            if (userDate.userType != 'admin') {
+                console.log('admin hai ');
+                window.location.replace('/screens/acounts.html');
+            }
+        }
+    });
+
+});
+
+const tBody = document.getElementById('tbody')
+
+
+
+
+const getUsers = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"))
+    querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        const userData = doc.data()
+        if (userData.userType != 'admin') {
+            const tBodyContant = `<tr>
+            <td>
+            <img src=${userData.userImage} class='image-usre'/>
+            </td>
+ 
+            <td>${userData.userName}</td>
+            <td>${userData.userEmail}</td>
+            <td>${userData.userNumber}</td>
+            <td>${userData.userType}</td>
+            <td>${userData.AcountActivate ? `<div class="form-check form-switch">
+                <input class="form-check-input" id=${userData.uid}  onchange={handleAccountActivation(this)} type="checkbox"  checked>
+                </div>` : `<div class="form-check form-switch">
+                <input class="form-check-input" id=${userData.uid}  onchange={handleAccountActivation(this)} type="checkbox" id="flexSwitchCheckChecked" >
+                </div>`
+
+                }</td >
+            
+                </tr>`
+            tBody.innerHTML += tBodyContant;
+        }
+
+    });
+
+}
+window.handleAccountActivation = async function handleAccountActivation(e) {
+    // console.log("handleAccountActivation", e.checked)
+    // console.log("handleAccountActivation", e.id)
+
+    try {
+        const userRef = doc(db, "users", e.id);
+        await updateDoc(userRef, {
+            AcountActivate: e.checked
+        })
+
+    } catch (error) {
+        alert(error.message)
+        // console.log(error.message)
+    }
+
+
+}
+
+
+// logOut Acount
+
+const logOutAcount = () => {
+    signOut(auth).then(() => {
+        swal("signOut", 'seccessfully signOut', "success");
+        setTimeout(() => {
+            window.location.replace('/screens/acounts.html')
+        }, 2000);
+
+    }).catch((error) => {
+        const errorMessage = error.message
+        swal("Oops", errorMessage, "error")
+
+    });
+}
+window.logOutAcount = logOutAcount
+
+
---
+++ js/customer.js
@@ -0,0 +1,76 @@
+import { auth, db, doc, getDoc, onAuthStateChanged, signOut } from "../config/Firebase.js";
+
+
+
+const user_profile_img_name = document.getElementById('user-profile-img-name');
+const user_profile_email = document.getElementById('user-profile-email');
+const user_profile_number = document.getElementById('user-profile-number');
+const user_profile_type = document.getElementById('user-profile-type');
+const user_profile_acount = document.getElementById('user-profile-acount');
+
+
+window.addEventListener("load", (event) => {
+
+    onAuthStateChanged(auth, async (user) => {
+        if (!user) {
+            window.location.replace('/screens/acounts.html');
+            return false;
+        }
+        const uid = user.uid
+        const docRef = doc(db, "users", uid);
+        const docSnap = await getDoc(docRef);
+        console.log("Document data:", docSnap.data());
+        const userDate = docSnap.data();
+        const userProfileDetail = `
+        <img src=${userDate.userImage}
+        alt="Avatar" class="img-fluid my-5" />
+        <h5 style="color: #fff;">${userDate.userName}</h5>
+        <i class="far fa-edit mb-5"></i>
+        `
+        const userProfileEmail = `
+        <p class="text-muted">${userDate.userEmail}</p>
+        `
+
+        const userProfileNumber = `
+        <p class="text-muted">${userDate.userNumber}</p>
+        `
+        const userProfileType = `
+        <p class="text-muted">${userDate.userType}</p>
+        `
+
+        const userProfileAcount = `
+        <p class="text-muted">${userDate.AcountActivate}</p>
+        `
+
+        user_profile_img_name.innerHTML += userProfileDetail;
+        user_profile_email.innerHTML += userProfileEmail;
+        user_profile_number.innerHTML += userProfileNumber;
+        user_profile_type.innerHTML += userProfileType;
+        user_profile_acount.innerHTML += userProfileAcount
+
+        if (userDate.userType != 'Customer') {
+            console.log('Customer nai ye ');
+            // window.location.replace('/screens/acounts.html');
+            history.back()
+        }
+    });
+});
+
+
+
+// logOut Acount
+
+const logOutAcount = () => {
+    signOut(auth).then(() => {
+        swal("signOut", 'seccessfully signOut', "success");
+        setTimeout(() => {
+            window.location.replace('/screens/acounts.html')
+        }, 2000);
+
+    }).catch((error) => {
+        const errorMessage = error.message
+        swal("Oops", errorMessage, "error")
+
+    });
+}
+window.logOutAcount = logOutAcount
